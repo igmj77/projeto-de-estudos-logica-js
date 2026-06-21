8dd3edf7aa7ae1c5e177130dbd8f6e64a3b4abbf
Israel <israelmelo77@gmail.com>
2026-06-21 20:15:53 -0300

    atualização; do, while end for

---
+++ Exercicios/aula20/ambiente.js
@@ -0,0 +1,18 @@
+/*
+var c = 1
+do {
+    console.log(`passo ${c}`)
+}   while (c <= 6)
+*/
+
+/*
+for (var c = 1;c <= 5;c++) {
+
+}
+*/
+
+console.log('Vai começar...')
+for (var c = 1;c <= 4;c++) {
+    console.log(c)
+}
+console.log('FIM')
